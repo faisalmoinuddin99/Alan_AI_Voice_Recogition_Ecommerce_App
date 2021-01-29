3205f3d54f2edfdab14deda8b38c400ffd98a118
faisal <faisal25march99@gmail.com>
2021-01-30 05:15:59 +0530

    Done with open cart with voice functionality

--- src/hooks/useAlan.js
+++ src/hooks/useAlan.js
@@ -1,21 +1,48 @@
-import {useEffect, useState} from 'react'
-import alanBtn from '@alan-ai/alan-sdk-web';
+import { useCallback, useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useCart } from "../context/CartContext";
+
+const COMMANDS = {
+  OPEN_CART: "open-cart",
+};
 
 export default function useAlan() {
-  
-  const [alanInstance, setAlanInstance] = useState()
+  const [alanInstance, setAlanInstance] = useState();
+  const { setShowCartItems, isCartEmpty } = useCart();
 
-  useEffect(()=>{
-    if(alanInstance != null) return
-  setAlanInstance( 
-    alanBtn({
-  top: "15px",
-  left : "15px",
-  key:'57e8946c228b23ec8d3d743c048476172e956eca572e1d8b807a3e2338fdd0dc/stage', 
-  onCommand : (commandData) => {
-    console.log(commandData);
-  }
-}))
-  }, [])
-  return null
+  const openCart = useCallback(() => {
+    if (isCartEmpty) {
+      alanInstance.playText(
+        "faisal sir! apka cart empty hai, please kuch item purchase kijye."
+      );
+    } else {
+      alanInstance.playText(
+        "jee malik faisal sir!  maine, right hand side, mai khool diya hai"
+      );
+      setShowCartItems(true);
+    }
+  }, [alanInstance, setShowCartItems, isCartEmpty]);
+
+  useEffect(() => {
+    window.addEventListener(COMMANDS.OPEN_CART, openCart);
+
+    return () => {
+      window.removeEventListener(COMMANDS.OPEN_CART, openCart);
+    };
+  }, [openCart]);
+
+  useEffect(() => {
+    if (alanInstance != null) return;
+    setAlanInstance(
+      alanBtn({
+        top: "15px",
+        left: "15px",
+        key: process.env.REACT_APP_ALAN_KEY,
+        onCommand: ({ command }) => {
+          window.dispatchEvent(new CustomEvent(command));
+        },
+      })
+    );
+  }, []);
+  return null;
 }
